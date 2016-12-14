a665a4329a1a9683b011a6215c6c65d6b359b1f4
kamel912 <mk.25390@gmail.com>
2016-12-14 12:21:10 +0200

    mm

--- app/src/main/java/com/mkapps/laliga/Clubs.java
+++ app/src/main/java/com/mkapps/laliga/Clubs.java
@@ -5,14 +5,13 @@ package com.mkapps.laliga;
  */
 
 public class Clubs {
-    String name,position,stadium;
+    String name,position;
     int emblem,points;
 
-    public Clubs(String n, String p,String s, int o, int e) {
+    public Clubs(String n, String p, int o, int e) {
         this.name = n;
         this.position = p;
         this.emblem = e;
         this.points = o;
-        this.stadium= s;
     }
 }
--- app/src/main/java/com/mkapps/laliga/MainActivity.java
+++ app/src/main/java/com/mkapps/laliga/MainActivity.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,32 +18,53 @@ ListView lV;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lV = (ListView) findViewById(R.id.listView);
-        LayoutInflater myInflater = getLayoutInflater();
-        View myView = myInflater.inflate(R.layout.list_view,(ViewGroup)findViewById(R.id.clubViews));
+        ArrayList<Clubs> clubs = new ArrayList<Clubs>();
+
+
+        customAdapter adapter = new customAdapter(clubs);
+
+        ListView listView = (ListView) findViewById(R.id.clubListView);
     }
     public class customAdapter extends BaseAdapter {
+        ArrayList<Clubs> clubs = new ArrayList<Clubs>();
+        customAdapter(ArrayList<Clubs> clubs){
+            this.clubs = clubs;
+        }
         @Override
         public int getCount() {
-            return 0;
+
+            return clubs.size();
         }
 
         @Override
         public Object getItem(int i) {
-            return null;
+
+            return clubs.get(i);
         }
 
         @Override
         public long getItemId(int i) {
-            return 0;
+
+            return i;
         }
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             return null;
         }
+        LayoutInflater myInflater = getLayoutInflater();
+        View myView = myInflater.inflate(R.layout.list_view,null);
+        TextView textName = (TextView) myView.findViewById(R.id.clubName);
+        TextView textPosition = (TextView) myView.findViewById(R.id.clubPostion);
+        TextView textPoints = (TextView) myView.findViewById(R.id.clubPoints);
+        ImageView imageEmblem = (ImageView) myView.findViewById(R.id.clubEmblem);
 
-        ArrayList<Clubs> clubses = new ArrayList<Clubs>();
+        Clubs temp = clubs.get(i);
+
+        textName.setText(temp.name);
+        textPosition.setText(temp.position);
+        textPoints.setText(temp.points);
+        imageEmblem.setImageResource(temp.emblem);
 
     }
 
